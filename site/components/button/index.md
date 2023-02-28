1945132468041385fdbb26b1aa327c49ecb22271
adekang <adekang@163.com>
2023-02-28 20:46:19 +0800

    feat: site

---
+++ site/components/button/index.md
@@ -0,0 +1,7 @@
+# Button 按钮
+
+按钮用于开始一个即时操作。
+
+## 演示例子
+
+<demo src="../../demo/button/basic.vue"></demo>
---
+++ site/demo/button/basic.vue
@@ -0,0 +1,14 @@
+<docs>
+---
+title:基础按钮
+---
+
+这是一个button按钮
+
+</docs>
+
+<script lang="ts" setup></script>
+
+<template>
+  <div>按钮</div>
+</template>
